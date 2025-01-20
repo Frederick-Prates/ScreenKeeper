a8c846b802e40b1f873e4a9e90caabfec081c59a
Fred Prates <fredprates@gmail.com>
2025-01-20 01:29:59 -0300

    black formatting

--- screenkeeper.py
+++ screenkeeper.py
@@ -1,15 +1,16 @@
 import ctypes
 import time
+
 mouse_event = ctypes.windll.user32.mouse_event
 MOUSE_EVENT_MODE = 0x0001
 timer = 10
 movement = 2
-print('press ctrl + c to exit')
+print("press ctrl + c to exit")
 try:
     while True:
-        mouse_event(MOUSE_EVENT_MODE,movement,0,0,0,0)#right
+        mouse_event(MOUSE_EVENT_MODE, movement, 0, 0, 0, 0)  # right
         time.sleep(timer)
-        mouse_event(MOUSE_EVENT_MODE,-movement,0,0,0,0)#left
+        mouse_event(MOUSE_EVENT_MODE, -movement, 0, 0, 0, 0)  # left
         time.sleep(timer)
 except KeyboardInterrupt:
     pass
